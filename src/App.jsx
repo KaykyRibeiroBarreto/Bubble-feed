72dfc1bb98a781edda4198378afc66c5ff299427
Kayky Ribeiro <142861320+KaykyRibeiroBarreto@users.noreply.github.com>
2025-03-13 15:03:29 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from './components/Header';
+import './styles/global.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
